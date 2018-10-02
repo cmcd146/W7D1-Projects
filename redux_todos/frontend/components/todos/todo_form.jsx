37f9969eb25b5b8c76111ffce18d2d243cfc3026
chris_mcdonald <mcdonaldchristopherkevin@gmail.com>
2018-10-01 17:08:49 -0700

    Add todo form

---
+++ redux_todos/frontend/components/todos/todo_form.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class ToDoForm extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      formVal: ''
+    };
+  }
+
+  render () {
+    return (
+
+
+    );
+  }
+
+}
